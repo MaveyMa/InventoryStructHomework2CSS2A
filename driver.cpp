e38d1b4c791bd32b05083a640a0030e4af1bba5f
Mavey Ma <mma@csumb.edu>
2016-02-11 15:39:17 -0800

    Wrote Inventory struct above the Main

--- driver.cpp
+++ driver.cpp
@@ -1,18 +1,51 @@
 //Name: Mavey Ma
 //Created: Thursday, February 11, 2016
 //CSS2A | Homework2 | Inventory Struct
+/* Define an Inventory struct type.
+1. Create a program that contains an array of 50 Inventory items.
+2. Initialize all 50 items with default values 
+(empty strings for the string variables, 0 for the number variables).
+3. Menu function
+	D - Display
+		// Display all Inventory items. Do not display Inventory that is empty
+	A - Add Inventory Item
+		// Prompt user for all information. Read input and set up next available Inventory item 
+		HINT: You need to keep track of how may Inventory items have been set up
+	U - Update an Inventory item
+		// Prompt user for a partNumber and use that to locate the Inventory item to update. 
+	I - Display one Inventory item 
+		// Prompt user for partNumber and display all information for that Inventory item
+	Q - Quit
+*/
 
 #include<iostream>
 #include<cstdlib>
 #include<string>
 using namespace std;
 
+struct Inventory {
+	string partName;
+	int partNumber;
+	double price;
+	int amountInStock;
+	int amountToReorder;
+};
+
 int main()
 {
+
+
+
+
 	cout << "HELLO PETER PANDA!" << endl;
-	for (int i = 0; i < 5; i++)
-	{
-		cout << i << " yummy bread" << endl;
-	}
 	return 0;
 }//END MAIN
+
+
+
+
+
+
+
+
+
