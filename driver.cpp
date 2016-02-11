5d7eb16c2eabda0bef7f8965dc377f32dff5b877
Mavey Ma <mma@csumb.edu>
2016-02-11 14:06:41 -0800

    Now, open sesame.

---
+++ driver.cpp
@@ -0,0 +1,14 @@
+//Name: Mavey Ma
+//Created: Thursday, February 11, 2016
+//CSS2A | Homework2 | Inventory Struct
+
+#include<iostream>
+#include<cstdlib>
+#include<string>
+using namespace std;
+
+int main()
+{
+	cout << "HELLO PETER PANDA!" << endl;
+	return 0;
+}//END MAIN
