00a3bdda1d3c2d7316f4fc5b737d1e87fc60e7b0
Mavey Ma <mma@csumb.edu>
2016-02-11 22:32:44 -0800

    Display function tested and passed.

--- driver.cpp
+++ driver.cpp
@@ -17,8 +17,8 @@ Menu function:
 */
 
 #include<iostream>
-#include<cstdlib>
 #include<string>
+#include<iomanip> //setw()
 using namespace std;
 
 struct Inventory {
@@ -30,23 +30,46 @@ struct Inventory {
 };
 
 void initialize(Inventory ary[], int n);
+//SUMMARY: Show all Inventory items, but not empty ones.
+//PRE: Takes in an Inventory struct type array, and an integer size of that array.
+//POST: cout << to screen relevant information.
+
+void display(Inventory ary[], int n);
 //SUMMARY: Initializes array items with default values.
 //PRE: Takes in an Inventory struct type array, and an integer size of that array.
 //POST: Within array, string variables become empty "" and number variables become 0.
+
+void addItem(Inventory ary[], int n);
+//SUMMARY: Prompt user for info. Read and write input.
+//PRE: Takes in an Inventory struct type array, and an integer size of that array.
+//POST: Adds user info of item into inventory.
 
 int main()
 {
 	const int SIZE = 50;
 	Inventory itemAry[SIZE];
-
-	
 	initialize(itemAry, SIZE);
-	cout << itemAry[0].partName << endl;
-	cout << itemAry[1].partNumber << endl;
-	cout << itemAry[3].price << endl;
-	cout << itemAry[33].amountInStock << endl;
-	cout << itemAry[49].amountToReorder << endl;
 
+	itemAry[0].partName = "Sword";
+	itemAry[0].partNumber = 13;
+	itemAry[0].price = 20.99;
+	itemAry[0].amountInStock = 2;
+	itemAry[0].amountToReorder = 9;
+
+	itemAry[1].partName = "Arrow";
+	itemAry[1].partNumber = 14;
+	itemAry[1].price = 1.99;
+	itemAry[1].amountInStock = 25;
+	itemAry[1].amountToReorder = 1;
+
+	itemAry[2].partName = "Javelin";
+	itemAry[2].partNumber = 6;
+	itemAry[2].price = 123.50;
+	itemAry[2].amountInStock = 2;
+	itemAry[2].amountToReorder = 9;
+
+
+	display(itemAry, SIZE);
 	cout << "HELLO PETER PANDA!" << endl;
 	return 0;
 }//END MAIN
@@ -57,13 +80,63 @@ void initialize(Inventory ary[], int n)
 	{
 		ary[i].partName = "";
 		ary[i].partNumber = 0;
-		ary[i].price = 0;
 		ary[i].amountInStock = 0;
 		ary[i].amountToReorder = 0;
-	}//END FOR LOOP
+		//ENSURE PRICE IS SHOWN TRADITIONALLY. e.g. $0.00
+		cout.setf(ios::fixed);
+		cout.setf(ios::showpoint);
+		cout.precision(2);
+		ary[i].price = 0;
+	}//END FOR
 	return;
 }//END INITIALIZE DEFAULT VALUES
 
+void display(Inventory ary[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (ary[i].partName == "")
+		{
+			return;
+		}
+		else
+		{
+			cout << "Name: " << ary[i].partName << endl;
+			cout << "Number: " << "#" << ary[i].partNumber << endl;
+			cout << "Price: " << "$" << ary[i].price << endl;
+			cout << "Stock: " << ary[i].amountInStock << endl;
+			cout << "Reorder: " << ary[i].amountToReorder << endl;
+		}
+	}//END FOR
+	return;
+}//END DISPLAY INVENTORY
+
+void addItem(Inventory ary[], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		if (ary[i].partName != "")
+		{
+			//LOOKING FOR NEXT EMPTY SLOT.
+			//IF NOT EMPTY (OCCUPIED), GO TO NEXT.
+			continue;
+		}
+		else
+		{
+			cout << "Enter Item Name: ";
+			cin >> ary[i].partName;
+			cout << "Enter Item Number: #";
+			cin >> ary[i].partNumber;
+			cout << "Enter Price: " << "$";
+			cin >> ary[i].price;
+			cout << "Enter Stock Count: ";
+			cin >> ary[i].amountInStock;
+			cout << "Enter Reorder Count: ";
+			cin >> ary[i].amountToReorder;
+		}
+	}//END FOR
+	return;
+}//END ADD ITEM
 
 
 
